617631449277b3e55093fa1d24722c851f346a77
Marcelo Selong <marceloselong1@gmail.com>
2025-09-29 18:31:02 -0300

    Deleção de arquivos que não serão mais utilizados

--- testes.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-with sqlite3.connect("database/banco.db") as conexao:
-    cursor = conexao.cursor()
-    cursor.execute("""
-        DELETE FROM manutencoes
-    """)
-    cursor.execute("""
-        DELETE FROM sqlite_sequence WHERE name='manutencoes'
-    """)
