1cc67b4797f13fc080a353bc02c55922e026a91d
Marcelo S. da Silva <marceloselong1@gmail.com>
2026-08-12 19:58:16 -0300

    Modificado o controller.py, para utilizar rotas com o Flask. Criado o template de home e o template que mostra as manutenções já existentes do veículo selecionado.

--- controller.py
+++ controller.py
@@ -1,183 +1,31 @@
-import arquivos, view, validacoes
-from flask import Flask, render_template, request
+import models
+from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
-@app.route("/")
-def homepage():
-    km_atual = arquivos.ultima_quilometragem()
-    return render_template("home.html", km=km_atual)
+app.secret_key = 'sua_chave_secreta'
 
-@app.route("/verificar-placa", methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route("/buscar", methods=['POST'])
 def verificar_placa():
-    placa_recebida = request.form.get('placa_input')
-    veiculo_encontrado = arquivos.verificar_placa(placa_recebida)
-    return render_template("manutencoes.html", veiculo=veiculo_encontrado)
+    cadastrar = request.args.get('cadastrar')
+    placa_url = request.args.get('placa', '').strip().upper()
+    if request.method == 'POST':
+        placa_recebida = request.form.get('placa', '').upper().strip()
+        veiculo, manutencoes = models.verificar_placa(placa_recebida)
 
+        if veiculo:
+            return render_template("manutencoes.html", veiculo=veiculo, manutencoes=manutencoes )
+        else:
+            flash(f'A placa "{placa_recebida}" não foi encontrada no sistema.', 'warning')
+            return render_template('home.html', placa_nao_encontrada=placa_recebida)
+    # Se o usuário clicou no link "Sim, cadastrar"
+    exibir_form = True if cadastrar == 'sim' else False
+    return render_template('home.html', placa_buscada=placa_url, exibir_formulario=exibir_form)
+    
 def executar_controller():
-    arquivos.inicializar_arquivo()
-
-
+    models.inicializar_arquivo()
 if __name__ == "__main__":
     executar_controller()
     app.run(debug=True)
-
-
-
-
-
-   
-    
-"""def executar_controller():
-    arquivos.inicializar_arquivo()
-    while True:
-        escolha = view.menu()
-        while True: #Faz a validação da escolha do usuário pra saber qual processo o código irá realizar
-            validacao, escolha = validacoes.validar_escolha_do_menu(escolha)
-            if validacao == True:
-                break
-            else:
-                escolha = view.reescolha_do_menu()
-        
-        if escolha == 1: #ESCOLHA DE CRIAR NOVA MANUTENÇÃO
-            placa = view.pedir_placa()
-            while True: #Faz a validação da placa
-                validacao, placa = validacoes.validar_placa(placa)
-                if validacao:
-                    break
-                else:
-                    placa = view.reescolha_da_placa()       
-            
-            dados = arquivos.carregar_placas()
-            
-            if validacoes.verificar_placa_nos_dados(placa, dados): #Se a placa já existir no sistema, cadastraremos uma nova manutenção nele.
-                id_veiculo = arquivos.retorno_id_veiculo(placa)
-                nova_manutencao(id_veiculo)
-            else: #Se a placa não existir no sistema, pergunta se deseja cadastrar novo veículo.
-                escolha = view.escolha_de_cadastrar_novo_veiculo()
-                while True: # Valida a escolha de cadastrar novo veículo
-                    validacao = validacoes.validar_escolha_de_cadastrar_novo_veiculo(escolha) 
-                    if validacao:
-                        break
-                    else:
-                        escolha = view.reescolha_de_cadastrar_novo_veiculo()
-                if escolha == "S": #Cadastra novo veículo
-                    modelo, marca, ano, quilometragem_atual = view.cadastro_novo_veiculo() #Entrada de dados pro cadastrodo novo veículo.
-                    while True: #Valida o ano do veículo e salva os dados no arquivo. 
-                        validacao, ano = validacoes.validar_ano(ano)
-                        if validacao:
-                            id_veiculo = arquivos.salvar_veiculo(placa, modelo, marca, ano, quilometragem_atual) #Puxa a placa do início, onde foi informada pra verificação no sistema
-                            if id_veiculo == 0:
-                                break
-                            else:
-                                break
-                        else:
-                            ano = view.reescolha_ano()
-                    if id_veiculo == 0: #Se ocorrer erro no salvamento do veículo no arquivo, pula a iteração e retorna pro início do laço novamente.
-                        continue
-
-                    #---------------CADASTRO DA NOVA MANUTENÇÃO--------------# 
-                    nova_manutencao(id_veiculo) 
-                else: #Retorna ao menu de escolha
-                    pass
-
-        elif escolha == 2: #LISTA AS MANUTENÇÕES DO VEÍCULO ESCOLHIDO
-            placa = view.pedir_placa()
-            while True: #Faz a validação da placa
-                validacao, placa = validacoes.validar_placa(placa)
-                if validacao:
-                    break
-                else:
-                    placa = view.reescolha_da_placa()
-            dados = arquivos.carregar_dados(placa)
-            if not dados:
-                print("\nNão existem manutenções no veículo com esta placa.\n")
-            else:
-                view.listar_manutencoes(dados, arquivos.tamanho_str_descricao())
-
-        elif escolha == 3:#DELETA A MANUTENÇÃO ESCOLHIDA DO VEÍCULO
-            placa = view.pedir_placa()
-            while True: #Faz a validação da placa
-                validacao, placa = validacoes.validar_placa(placa)
-                if validacao:
-                    break
-                else:
-                    placa = view.reescolha_da_placa()
-            dados = arquivos.carregar_dados(placa)
-            
-            view.listar_manutencoes(dados, arquivos.tamanho_str_descricao())
-            if dados != []:
-                escolha = view.escolha_deletar_manutencao()
-                while True:
-                    validacao, escolha = validacoes.validar_escolha_de_deletar_manutencao(escolha, dados)
-                    if validacao:
-                        arquivos.deletar_manutencao(escolha)
-                        break
-                    else:
-                        escolha = view.reescolha_deletar_manutencao()
-                
-        elif escolha == 0:
-            quit()
-executar_controller()"""
-
-
-"""def nova_manutencao(id_veiculo):
-    continuar_informando_servicos = "S"
-    while continuar_informando_servicos == "S": #Laço pra informar os serviços
-        km, data = view.km_data_nova_manutencao() #Entrada da data e km           #KM E DATA
-        while True: #Validação da km
-            validacao, km = validacoes.validacao_do_km(km)
-            if validacao:
-                break
-            else: 
-                km = view.reentrada_da_quilometragem()   
-        while True: #Validação da data
-            validacao, data = validacoes.validacao_da_data(data) 
-            if validacao:
-                break
-            else:
-                data = view.reentrada_da_data()       
-        descricao, custo_str = view.entrada_do_servico()                             #DESCRIÇÃO DO SERVIÇO
-        while True: #Valida se é um valor numérico.                               #CUSTO DO SERVIÇO
-            validacao, custo_float = validacoes.validar_custo(custo_str)
-            if validacao:
-                break
-            else:
-                custo = view.reentrada_custo()
-
-        id_servico = arquivos.salvar_servico(id_veiculo, descricao, custo_float, data, km) #Salva o serviço realizado no BD e retorna o ID dele
-        if id_servico == 0: #Se o retorno do ID da manutenção for 0, ocorreu um erro no salvamento. Então, pula o resto do laço.
-            print("Informe novamente a manutenção.")
-            continue
-        else:
-            continuar_informando_pecas = "S"
-            while continuar_informando_pecas == "S": #Laço para informar as peças.
-                descricao_da_peca, marca_da_peca, valor_da_peca_str = view.entrada_pecas()   #DESCRIÇÃO DA PEÇA
-                while True: #Validação do valor da peça   #VALOR DA PEÇA
-                    validacao, valor_da_peca_float = validacoes.validar_valor_da_peca(valor_da_peca_str)
-                    if validacao:
-                        break
-                    else:
-                        valor_da_peca_str = view.reentrada_valor_da_peca()
-                retorno = arquivos.salvar_peca(id_servico, descricao_da_peca, marca_da_peca, valor_da_peca_float)
-                if retorno == 0:
-                    print("Repita a operação.")
-                    continue
-                else:
-                    print(retorno)
-                continuar_informando_pecas = view.informar_mais_pecas()
-                while True: #Validação da escolha de adicionar mais peças
-                    validacao, continuar_informando_pecas = validacoes.validar_escolha(continuar_informando_pecas)
-                    if validacao:
-                        break
-                    else:
-                        continuar_informando_pecas = view.reescolha_informar_mais()
-
-
-        continuar_informando_servicos = view.informar_mais_servicos() 
-        while True: #Validação da escolha de adicionar mais serviços
-            validacao, continuar_informando_servicos = validacoes.validar_escolha(continuar_informando_servicos)
-            if validacao:
-                break
-            else:
-                continuar_informando_servicos = view.reescolha_informar_mais()"""
-
-
