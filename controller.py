ad3b1674d4cb976e3dec36ad2ad26cd9e5b594ce
Marcelo S. da Silva <marceloselong1@gmail.com>
2026-08-12 21:41:52 -0300

    Correção da rota /buscar, retornando aos conhecimentos existentes.

--- controller.py
+++ controller.py
@@ -1,29 +1,27 @@
 import models
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request
 app = Flask(__name__)
 app.secret_key = 'sua_chave_secreta'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    #TODO Refatorar, criar a parte do Warning e condição se não encontrar veículo no BD.
     return render_template('home.html')
 
 @app.route("/buscar", methods=['POST'])
 def verificar_placa():
-    cadastrar = request.args.get('cadastrar')
-    placa_url = request.args.get('placa', '').strip().upper()
-    if request.method == 'POST':
-        placa_recebida = request.form.get('placa', '').upper().strip()
-        veiculo, manutencoes = models.verificar_placa(placa_recebida)
+    placa_recebida = request.form.get('placa', '').upper().strip()
+    veiculo, manutencoes = models.verificar_placa(placa_recebida) #Se a placa existir no BD, retorna os dados do veículo e suas manutenções. Se não, retorna None e uma lista vazia.
+    if veiculo:
+        return render_template('manutencoes.html', veiculo=veiculo, manutencoes=manutencoes)
+    else:
+        #TODO Refatorar, criar a parte do Warning e condição se não encontrar veículo no BD. 
+        return render_template('home.html', placa=placa_recebida)
 
-        if veiculo:
-            return render_template("manutencoes.html", veiculo=veiculo, manutencoes=manutencoes )
-        else:
-            flash(f'A placa "{placa_recebida}" não foi encontrada no sistema.', 'warning')
-            return render_template('home.html', placa_nao_encontrada=placa_recebida)
-    # Se o usuário clicou no link "Sim, cadastrar"
-    exibir_form = True if cadastrar == 'sim' else False
-    return render_template('home.html', placa_buscada=placa_url, exibir_formulario=exibir_form)
-    
+
+
+
+
 def executar_controller():
     models.inicializar_arquivo()
 if __name__ == "__main__":
